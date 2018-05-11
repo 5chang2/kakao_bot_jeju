1962672ef05835b754457c8832ff39344fbc8624
EC2 Default User <ec2-user@ip-172-31-12-61.us-east-2.compute.internal>
2018-05-11 10:16:34 +0000

    keyboard 완성

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def keyboard
+    @keyboard = {
+      type: "buttons",						# 이렇게 작성해도
+      buttons: ["선택 1", "선택 2", "선택 3"]		 # 요렇게 작성해도 똑같습니다.
+    }
+    render json: @keyboard
+  end
+end
